96430a7b2834c138604b6e9aef6a33e2246ff792
Jadie-Wadie <49811314+Jadie-Wadie@users.noreply.github.com>
2020-08-10 18:01:22 +0930

    initialise project

---
+++ Assets/Scripts/TestScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+	void Start()
+	{
+		Debug.Log("Hello World!");
+	}
+
+	void Update()
+	{
+
+	}
+}
