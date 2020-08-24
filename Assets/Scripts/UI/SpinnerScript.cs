717ceff9e975e7ff0a3b248c99d9645947d03045
Jadie Wadie <jaidentdouglas@gmail.com>
2020-08-24 20:28:22 +0930

    begin Loading Screen

---
+++ Assets/Scripts/UI/SpinnerScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinnerScript : MonoBehaviour
+{
+	[Header("General")]
+	public float speed = 540f;
+
+	void Update()
+	{
+		transform.Rotate(new Vector3(0, 0, Time.deltaTime * speed));
+	}
+}
