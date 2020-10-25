b8294d147353a9d1dd8395173bfc7aab3a4f4ad3
Jadie Wadie <jaidentdouglas@gmail.com>
2020-10-25 16:34:10 +1030

    Add InfoScript

---
+++ Assets/Scripts/UI/InfoScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using Photon.Pun;
+
+public class InfoScript : MonoBehaviour
+{
+	[Header("Debug")]
+	private TMP_Text text;
+
+	void Awake()
+	{
+		text = GetComponent<TMP_Text>();
+	}
+
+	void Update()
+	{
+		int players = PhotonNetwork.CountOfPlayers;
+		text.text = $"{PhotonNetwork.CloudRegion}";
+	}
+}
