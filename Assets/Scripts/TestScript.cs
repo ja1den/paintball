8a4ec9a122a016a40886158f32c12d3ab77e51e9
Jadie Wadie <jaidentdouglas@gmail.com>
2021-01-12 10:29:10 +1030

    chore: initialise project

---
+++ Assets/Scripts/TestScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+	public string message = "Hello, world!";
+
+	void Start()
+	{
+		Debug.Log(message);
+	}
+}
