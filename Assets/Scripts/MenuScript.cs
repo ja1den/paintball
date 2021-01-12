6ece2692ad58a8824d9eb19048485921371cd260
Jadie Wadie <jaidentdouglas@gmail.com>
2021-01-12 20:51:00 +1030

    feat: begin Menu

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuScript : MonoBehaviour
+{
+	public void Play()
+	{
+		Debug.Log("Play Button");
+	}
+
+	public void Options()
+	{
+		Debug.Log("Options Button");
+	}
+}
