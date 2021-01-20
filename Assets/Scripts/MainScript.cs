950774d3ee57673a5e65f0eb3b5f54949a846265
Jadie Wadie <jaidentdouglas@gmail.com>
2021-01-20 22:47:11 +1030

    feat: add username UI

---
+++ Assets/Scripts/MainScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using MLAPI;
+using TMPro;
+
+public enum GameState
+{
+	Menu, Play
+}
+
+public class MainScript : NetworkedBehaviour
+{
+	[Header("UI")]
+	public TMP_InputField nameInput;
+	public Button playButton;
+
+	void Start()
+	{
+		// NetworkingManager.Singleton.StartClient();
+	}
+
+	public void NameInput()
+	{
+		playButton.interactable = nameInput.text.Length >= 3;
+	}
+
+	public void PlayButton()
+	{
+		Debug.Log("Joining as '" + nameInput.text + "'");
+	}
+}
